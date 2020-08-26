cd183eba13ceea01f7407fe832007fca0a531499
Hjalti Geir Atlason <hjalti@hjalti.me>
2020-08-26 14:17:40 +0000

    automated the ssh keygen process and added nodes to known host file to work with sshpass in a script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo apt install sshpass -y 
 
-
+ssh-keygen -t rsa -b 2048  -f /home/vagrant/.ssh/id_rsa
 
 echo " 
 
@@ -10,5 +11,9 @@ echo "
 10.10.10.11 node1
 10.10.10.12 node2
 10.10.10.13 node3
+10.10.10.14 redhat-node
 " >> /etc/hosts
 
+ssh-keyscan node1 node2 node3 redhat-node >> /home/vagrant/.ssh/known_hosts
+
+chown -R vagrant:vagrant /home/vagrant/.ssh
